488d08ad4b24266b4c75d131540d007de5ab59a0
Itsuki54 <104209850+Itsuki54@users.noreply.github.com>
2025-02-23 03:26:55 +0000

    Apply automatic changes

--- src/data/contest.ts
+++ src/data/contest.ts
@@ -1,191 +1,191 @@
 export const contests = [
-  {
-    date: "2022-04-23 22:40:00+0900-",
-    contest_name: "Monoxer Programming Contest 2022（AtCoder Beginner Contest 249）6488",
-    rank: "6488",
-    submissions: "51",
-    rating: "3",
-  },
-  {
-    date: "2023-04-09 22:40:00+0900+9",
-    contest_name: "AtCoder Beginner Contest 2976679",
-    rank: "6679",
-    submissions: "110",
-    rating: "12",
-  },
-  {
-    date: "2023-04-15 22:40:00+0900-",
-    contest_name: "TOYOTA MOTOR CORPORATION Programming Contest 2023#1 (AtCoder Beginner Contest 298)7174",
-    rank: "7174",
-    submissions: "-",
-    rating: "-",
-  },
-  {
-    date: "2023-06-03 23:00:00+0900-",
-    contest_name: "Tokio Marine & Nichido Fire Insurance Programming Contest 2023（AtCoder Beginner Contest 304)6750",
-    rank: "6750",
-    submissions: "-",
-    rating: "-",
-  },
-  {
-    date: "2023-07-02 23:00:00+0900+15",
-    contest_name: "AtCoder Regular Contest 1632363",
-    rank: "2363",
-    submissions: "165",
-    rating: "27",
-  },
-  {
-    date: "2023-07-15 22:40:00+0900+35",
-    contest_name: "freee Programming Contest 2023（AtCoder Beginner Contest 310）8453",
-    rank: "8453",
-    submissions: "478",
-    rating: "62",
-  },
-  {
-    date: "2023-10-28 22:40:00+0900+21",
-    contest_name: "Panasonic Programming Contest 2023（AtCoder Beginner Contest 326）7357",
-    rank: "7357",
-    submissions: "258",
-    rating: "83",
-  },
-  {
-    date: "2023-11-25 22:40:00+0900+31",
-    contest_name: "TOYOTA SYSTEMS Programming Contest 2023(AtCoder Beginner Contest 330)5655",
-    rank: "5655",
-    submissions: "409",
-    rating: "114",
-  },
-  {
-    date: "2023-12-09 23:00:00+0900+28",
-    contest_name: "estie Programming Contest 2023 (AtCoder Regular Contest 169)1711",
-    rank: "1711",
-    submissions: "393",
-    rating: "142",
-  },
-  {
-    date: "2023-12-10 22:40:00+0900+21",
-    contest_name: "AtCoder Beginner Contest 3325399",
-    rank: "5399",
-    submissions: "348",
-    rating: "163",
-  },
-  {
-    date: "2024-01-06 22:40:00+0900+48",
-    contest_name: "AtCoder Beginner Contest 335 (Sponsored by Mynavi)5119",
-    rank: "5119",
-    submissions: "648",
-    rating: "211",
-  },
-  {
-    date: "2024-01-20 22:40:00+0900+18",
-    contest_name: "Toyota Programming Contest 2024#1（AtCoder Beginner Contest 337）7546",
-    rank: "7546",
-    submissions: "382",
-    rating: "229",
-  },
-  {
-    date: "2024-01-21 23:00:00+0900+4",
-    contest_name: "AtCoder Regular Contest 1702058",
-    rank: "2058",
-    submissions: "263",
-    rating: "233",
-  },
-  {
-    date: "2024-01-27 22:40:00+0900+26",
-    contest_name: "AtCoder Beginner Contest 3385698",
-    rank: "5698",
-    submissions: "493",
-    rating: "259",
-  },
-  {
-    date: "2024-02-03 22:40:00+0900+35",
-    contest_name: "Japan Registry Services (JPRS) Programming Contest 2024 (AtCoder Beginner Contest 339)5655",
-    rank: "5655",
-    submissions: "600",
-    rating: "294",
-  },
-  {
-    date: "2024-02-10 22:40:00+0900+37",
-    contest_name: "KAJIMA CORPORATION CONTEST 2024（AtCoder Beginner Contest 340）4677",
-    rank: "4677",
-    submissions: "629",
-    rating: "331",
-  },
-  {
-    date: "2024-02-17 22:40:00+0900+14",
-    contest_name: "Toyota Programming Contest 2024#2（AtCoder Beginner Contest 341）6396",
-    rank: "6396",
-    submissions: "458",
-    rating: "345",
-  },
-  {
-    date: "2024-02-24 22:40:00+0900+54",
-    contest_name: "HUAWEI Programming Contest 2024（AtCoder Beginner Contest 342）4149",
-    rank: "4149",
-    submissions: "773",
-    rating: "399",
-  },
-  {
-    date: "2024-03-09 22:40:00+0900+162",
-    contest_name: "Toyota Programming Contest 2024#3（AtCoder Beginner Contest 344）1498",
-    rank: "1498",
-    submissions: "1376",
-    rating: "561",
-  },
-  {
-    date: "2024-03-23 22:40:00+0900+26",
-    contest_name: "UNIQUE VISION Programming Contest 2024 Spring(AtCoder Beginner Contest 346)4363",
-    rank: "4363",
-    submissions: "776",
-    rating: "587",
-  },
-  {
-    date: "2024-06-01 22:40:00+0900+117",
-    contest_name: "AtCoder Beginner Contest 3561658",
-    rank: "1658",
-    submissions: "1372",
-    rating: "704",
-  },
-  {
-    date: "2024-09-21 22:40:00+0900-14",
-    contest_name: "UNIQUE VISION Programming Contest 2024 Autumn (AtCoder Beginner Contest 372)5211",
-    rank: "5211",
-    submissions: "563",
-    rating: "690",
-  },
-  {
-    date: "2024-09-28 22:40:00+0900-27",
-    contest_name: "AtCoder Beginner Contest 3737073",
-    rank: "7073",
-    submissions: "420",
-    rating: "663",
-  },
-  {
-    date: "2024-10-05 22:40:00+0900+14",
-    contest_name: "KEYENCE Programming Contest 2024（AtCoder Beginner Contest 374）4549",
-    rank: "4549",
-    submissions: "787",
-    rating: "677",
-  },
-  {
-    date: "2024-10-19 22:40:00+0900-15",
-    contest_name: "AtCoder Beginner Contest 3766255",
-    rank: "6255",
-    submissions: "529",
-    rating: "662",
-  },
-  {
-    date: "2024-10-26 22:40:00+0900-",
-    contest_name: "TOYOTA SYSTEMS Programming Contest 2024（AtCoder Beginner Contest 377)7819",
-    rank: "7819",
-    submissions: "-",
-    rating: "-",
-  },
-  {
-    date: "2025-01-11 22:40:00+0900-",
-    contest_name: "HHKB Programming Contest 2025(AtCoder Beginner Contest 388)9548",
-    rank: "9548",
-    submissions: "-",
-    rating: "-",
-  },
+    {
+        "date": "2022-04-23 22:40:00+0900-",
+        "contest_name": "Monoxer Programming Contest 2022（AtCoder Beginner Contest 249）6488",
+        "rank": "6488",
+        "submissions": "51",
+        "rating": "3"
+    },
+    {
+        "date": "2023-04-09 22:40:00+0900+9",
+        "contest_name": "AtCoder Beginner Contest 2976679",
+        "rank": "6679",
+        "submissions": "110",
+        "rating": "12"
+    },
+    {
+        "date": "2023-04-15 22:40:00+0900-",
+        "contest_name": "TOYOTA MOTOR CORPORATION Programming Contest 2023#1 (AtCoder Beginner Contest 298)7174",
+        "rank": "7174",
+        "submissions": "-",
+        "rating": "-"
+    },
+    {
+        "date": "2023-06-03 23:00:00+0900-",
+        "contest_name": "Tokio Marine & Nichido Fire Insurance Programming Contest 2023（AtCoder Beginner Contest 304)6750",
+        "rank": "6750",
+        "submissions": "-",
+        "rating": "-"
+    },
+    {
+        "date": "2023-07-02 23:00:00+0900+15",
+        "contest_name": "AtCoder Regular Contest 1632363",
+        "rank": "2363",
+        "submissions": "165",
+        "rating": "27"
+    },
+    {
+        "date": "2023-07-15 22:40:00+0900+35",
+        "contest_name": "freee Programming Contest 2023（AtCoder Beginner Contest 310）8453",
+        "rank": "8453",
+        "submissions": "478",
+        "rating": "62"
+    },
+    {
+        "date": "2023-10-28 22:40:00+0900+21",
+        "contest_name": "Panasonic Programming Contest 2023（AtCoder Beginner Contest 326）7357",
+        "rank": "7357",
+        "submissions": "258",
+        "rating": "83"
+    },
+    {
+        "date": "2023-11-25 22:40:00+0900+31",
+        "contest_name": "TOYOTA SYSTEMS Programming Contest 2023(AtCoder Beginner Contest 330)5655",
+        "rank": "5655",
+        "submissions": "409",
+        "rating": "114"
+    },
+    {
+        "date": "2023-12-09 23:00:00+0900+28",
+        "contest_name": "estie Programming Contest 2023 (AtCoder Regular Contest 169)1711",
+        "rank": "1711",
+        "submissions": "393",
+        "rating": "142"
+    },
+    {
+        "date": "2023-12-10 22:40:00+0900+21",
+        "contest_name": "AtCoder Beginner Contest 3325399",
+        "rank": "5399",
+        "submissions": "348",
+        "rating": "163"
+    },
+    {
+        "date": "2024-01-06 22:40:00+0900+48",
+        "contest_name": "AtCoder Beginner Contest 335 (Sponsored by Mynavi)5119",
+        "rank": "5119",
+        "submissions": "648",
+        "rating": "211"
+    },
+    {
+        "date": "2024-01-20 22:40:00+0900+18",
+        "contest_name": "Toyota Programming Contest 2024#1（AtCoder Beginner Contest 337）7546",
+        "rank": "7546",
+        "submissions": "382",
+        "rating": "229"
+    },
+    {
+        "date": "2024-01-21 23:00:00+0900+4",
+        "contest_name": "AtCoder Regular Contest 1702058",
+        "rank": "2058",
+        "submissions": "263",
+        "rating": "233"
+    },
+    {
+        "date": "2024-01-27 22:40:00+0900+26",
+        "contest_name": "AtCoder Beginner Contest 3385698",
+        "rank": "5698",
+        "submissions": "493",
+        "rating": "259"
+    },
+    {
+        "date": "2024-02-03 22:40:00+0900+35",
+        "contest_name": "Japan Registry Services (JPRS) Programming Contest 2024 (AtCoder Beginner Contest 339)5655",
+        "rank": "5655",
+        "submissions": "600",
+        "rating": "294"
+    },
+    {
+        "date": "2024-02-10 22:40:00+0900+37",
+        "contest_name": "KAJIMA CORPORATION CONTEST 2024（AtCoder Beginner Contest 340）4677",
+        "rank": "4677",
+        "submissions": "629",
+        "rating": "331"
+    },
+    {
+        "date": "2024-02-17 22:40:00+0900+14",
+        "contest_name": "Toyota Programming Contest 2024#2（AtCoder Beginner Contest 341）6396",
+        "rank": "6396",
+        "submissions": "458",
+        "rating": "345"
+    },
+    {
+        "date": "2024-02-24 22:40:00+0900+54",
+        "contest_name": "HUAWEI Programming Contest 2024（AtCoder Beginner Contest 342）4149",
+        "rank": "4149",
+        "submissions": "773",
+        "rating": "399"
+    },
+    {
+        "date": "2024-03-09 22:40:00+0900+162",
+        "contest_name": "Toyota Programming Contest 2024#3（AtCoder Beginner Contest 344）1498",
+        "rank": "1498",
+        "submissions": "1376",
+        "rating": "561"
+    },
+    {
+        "date": "2024-03-23 22:40:00+0900+26",
+        "contest_name": "UNIQUE VISION Programming Contest 2024 Spring(AtCoder Beginner Contest 346)4363",
+        "rank": "4363",
+        "submissions": "776",
+        "rating": "587"
+    },
+    {
+        "date": "2024-06-01 22:40:00+0900+117",
+        "contest_name": "AtCoder Beginner Contest 3561658",
+        "rank": "1658",
+        "submissions": "1372",
+        "rating": "704"
+    },
+    {
+        "date": "2024-09-21 22:40:00+0900-14",
+        "contest_name": "UNIQUE VISION Programming Contest 2024 Autumn (AtCoder Beginner Contest 372)5211",
+        "rank": "5211",
+        "submissions": "563",
+        "rating": "690"
+    },
+    {
+        "date": "2024-09-28 22:40:00+0900-27",
+        "contest_name": "AtCoder Beginner Contest 3737073",
+        "rank": "7073",
+        "submissions": "420",
+        "rating": "663"
+    },
+    {
+        "date": "2024-10-05 22:40:00+0900+14",
+        "contest_name": "KEYENCE Programming Contest 2024（AtCoder Beginner Contest 374）4549",
+        "rank": "4549",
+        "submissions": "787",
+        "rating": "677"
+    },
+    {
+        "date": "2024-10-19 22:40:00+0900-15",
+        "contest_name": "AtCoder Beginner Contest 3766255",
+        "rank": "6255",
+        "submissions": "529",
+        "rating": "662"
+    },
+    {
+        "date": "2024-10-26 22:40:00+0900-",
+        "contest_name": "TOYOTA SYSTEMS Programming Contest 2024（AtCoder Beginner Contest 377)7819",
+        "rank": "7819",
+        "submissions": "-",
+        "rating": "-"
+    },
+    {
+        "date": "2025-01-11 22:40:00+0900-",
+        "contest_name": "HHKB Programming Contest 2025(AtCoder Beginner Contest 388)9548",
+        "rank": "9548",
+        "submissions": "-",
+        "rating": "-"
+    }
 ];
